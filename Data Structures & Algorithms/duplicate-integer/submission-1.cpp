2bcb4ae14b039af4399c62e6f84dff3cae372d69
GAJENDER <90421243+gajender09@users.noreply.github.com>
2026-05-16 00:58:17 +0530

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> mp;
+
+        for (int num : nums) {
+            mp[num]++;
+
+            if (mp[num] > 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
